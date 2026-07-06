8be7cf8b6b8cc3a88dffdd2a396bb70d8983058d
Cris <cristinaporteiro@gmail.com>
2026-07-06 18:23:41 +0200

    Created Login and Signup components.

---
+++ FRONTEND/src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import './Login.scss';
+import Header from '../../components/Header/Header';
+
+const Login = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Login;
---
+++ FRONTEND/src/pages/Signup/Signup.jsx
@@ -0,0 +1,12 @@
+import './Signup.scss';
+import Header from '../../components/Header/Header';
+
+const Signup = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Signup;
