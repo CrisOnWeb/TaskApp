f65445330393a27c16f4c7446da310d8c0c296d2
Cris <cristinaporteiro@gmail.com>
2026-07-14 16:48:38 +0200

    Refactor api.js to use async/await.

--- FRONTEND/src/services/api.js
+++ FRONTEND/src/services/api.js
@@ -1,21 +1,9 @@
 // url es la url del fetch y options es un objeto para poder pasarle el tipo de petición (GET, POST...), headers, datos, etc.
-const fetchData = (url, options = {}) => {
-  return fetch(url, options)
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error('Error en la petición');
-      }
-      return response.json();
-    })
-    .then((data) => {
-      /* Puedes filtrar los datos que te devuelve con un map
-        return data.map((user) => {
-        id: user.id,
-        name: user.name,
-      });
-      */
-      return data;
-    });
+const fetchData = async (url, options = {}) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
 };
 
 export default fetchData;
