1ca9475ff63b517bd4565d5606c72109c58a4385
Cris <cristinaporteiro@gmail.com>
2026-07-17 11:59:02 +0200

    Refactor: Moved TaskApp’s logic from App.jsx to TaskApp.jsx

--- FRONTEND/src/components/App.jsx
+++ FRONTEND/src/components/App.jsx
@@ -1,6 +1,4 @@
-import { useState, useEffect } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import ls from '../services/localStorage';
 import authService from '../services/authService';
 import TaskApp from '../pages/TaskApp/TaskApp';
 import Landing from '../pages/Landing/Landing';
@@ -8,87 +6,6 @@ import Login from '../pages/Login/Login';
 import Signup from '../pages/Signup/Signup';
 
 function App() {
-  const [tasks, setTasks] = useState(
-    ls.get('data', [
-      {
-        id: crypto.randomUUID(),
-        text: 'Aprender a utilizar TaskApp',
-        completed: false,
-      },
-      {
-        id: crypto.randomUUID(),
-        text: 'Crear una nueva tarea',
-        completed: false,
-      },
-      { id: crypto.randomUUID(), text: 'Eliminar una tarea', completed: false },
-    ])
-  );
-  const [newTaskInput, setNewTaskInput] = useState('');
-  const [search, setSearch] = useState('');
-  const [filter, setFilter] = useState('all');
-
-  useEffect(() => {
-    ls.set('data', tasks);
-  }, [tasks]);
-
-  const normalizeText = (text) => {
-    return text
-      .normalize('NFD') // Separa acentos
-      .replace(/[\u0300-\u036f]/g, '') // Elimina acentos
-      .toLowerCase() // Minúsculas
-      .trim(); // elimina espacios
-  };
-
-  const searchedTasks = (filteredTasks) => {
-    // Si search está vacío, devuelvo todas las tareas
-    if (!search.trim()) {
-      return filteredTasks;
-    } else {
-      // Si no, filtro por el contenido de search
-      const searchedText = normalizeText(search);
-      return filteredTasks.filter((task) =>
-        // Recojo las tareas que coincidan
-        normalizeText(task.text).includes(searchedText)
-      );
-    }
-  };
-
-  // Se filtran las tareas según su estado
-  const pendingTasks = tasks.filter((task) => !task.completed);
-  const completedTasks = tasks.filter((task) => task.completed);
-
-  // Se filtran por búsqueda de la usuaria
-  const searchedPendingTasks = searchedTasks(pendingTasks);
-  const searchedCompletedTasks = searchedTasks(completedTasks);
-
-  // Se calcula la cantidad de tareas según status
-  // Usamos las variables de búsqueda porque queremos que se actualice cuando buscamos
-  const numberOfPendingTasks = searchedPendingTasks.length;
-  const numberOfCompletedTasks = searchedCompletedTasks.length;
-  // Usamos las variables de tareas totales para el resumen
-  const pendingTasksSummary = pendingTasks.length;
-  const completedTasksSummary = completedTasks.length;
-
-  const addTask = (newTask) => {
-    setTasks([...tasks, newTask]);
-  };
-
-  const deleteTask = (id) => {
-    setTasks(tasks.filter((task) => task.id !== id));
-  };
-
-  const toggleTask = (id) => {
-    return setTasks(
-      tasks.map((task) =>
-        task.id === id ? { ...task, completed: !task.completed } : task
-      )
-    );
-  };
-
-  const handleFilterChange = (status) => {
-    setFilter(status);
-  };
-
   return (
     <>
       <Routes>
@@ -99,23 +16,7 @@ function App() {
           path="/app"
           element={
             authService.isAuthenticated() ? (
-              <TaskApp
-                search={search}
-                setSearch={setSearch}
-                newTaskInput={newTaskInput}
-                setNewTaskInput={setNewTaskInput}
-                addTask={addTask}
-                filter={filter}
-                handleFilterChange={handleFilterChange}
-                pendingTasksSummary={pendingTasksSummary}
-                completedTasksSummary={completedTasksSummary}
-                numberOfPendingTasks={numberOfPendingTasks}
-                numberOfCompletedTasks={numberOfCompletedTasks}
-                searchedPendingTasks={searchedPendingTasks}
-                searchedCompletedTasks={searchedCompletedTasks}
-                toggleTask={toggleTask}
-                deleteTask={deleteTask}
-              />
+              <TaskApp />
             ) : (
               <Navigate to="/login" replace />
             )
--- FRONTEND/src/pages/TaskApp/TaskApp.jsx
+++ FRONTEND/src/pages/TaskApp/TaskApp.jsx
@@ -1,4 +1,8 @@
 import './TaskApp.scss';
+import { useState, useEffect } from 'react';
+import tasksService from '../../services/tasksService';
+import authService from '../../services/authService';
+import ls from '../../services/localStorage';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 import NewTask from '../../components/NewTask/NewTask';
@@ -8,23 +12,87 @@ import CompletedIcon from '../../components/icons/CompletedIcon';
 import FilterTasks from '../../components/FilterTasks/FilterTasks';
 import TaskSummary from '../../components/TaskSummary/TaskSummary';
 
-const TaskApp = ({
-  search,
-  setSearch,
-  newTaskInput,
-  setNewTaskInput,
-  addTask,
-  filter,
-  handleFilterChange,
-  pendingTasksSummary,
-  completedTasksSummary,
-  numberOfPendingTasks,
-  numberOfCompletedTasks,
-  searchedPendingTasks,
-  searchedCompletedTasks,
-  toggleTask,
-  deleteTask,
-}) => {
+const TaskApp = () => {
+  const [tasks, setTasks] = useState(
+    ls.get('data', [
+      {
+        id: crypto.randomUUID(),
+        text: 'Aprender a utilizar TaskApp',
+        completed: false,
+      },
+      {
+        id: crypto.randomUUID(),
+        text: 'Crear una nueva tarea',
+        completed: false,
+      },
+      { id: crypto.randomUUID(), text: 'Eliminar una tarea', completed: false },
+    ])
+  );
+  const [newTaskInput, setNewTaskInput] = useState('');
+  const [search, setSearch] = useState('');
+  const [filter, setFilter] = useState('all');
+
+  useEffect(() => {
+    ls.set('data', tasks);
+  }, [tasks]);
+
+  const normalizeText = (text) => {
+    return text
+      .normalize('NFD') // Separa acentos
+      .replace(/[\u0300-\u036f]/g, '') // Elimina acentos
+      .toLowerCase() // Minúsculas
+      .trim(); // elimina espacios
+  };
+
+  const searchedTasks = (filteredTasks) => {
+    // Si search está vacío, devuelvo todas las tareas
+    if (!search.trim()) {
+      return filteredTasks;
+    } else {
+      // Si no, filtro por el contenido de search
+      const searchedText = normalizeText(search);
+      return filteredTasks.filter((task) =>
+        // Recojo las tareas que coincidan
+        normalizeText(task.text).includes(searchedText)
+      );
+    }
+  };
+
+  // Se filtran las tareas según su estado
+  const pendingTasks = tasks.filter((task) => !task.completed);
+  const completedTasks = tasks.filter((task) => task.completed);
+
+  // Se filtran por búsqueda de la usuaria
+  const searchedPendingTasks = searchedTasks(pendingTasks);
+  const searchedCompletedTasks = searchedTasks(completedTasks);
+
+  // Se calcula la cantidad de tareas según status
+  // Usamos las variables de búsqueda porque queremos que se actualice cuando buscamos
+  const numberOfPendingTasks = searchedPendingTasks.length;
+  const numberOfCompletedTasks = searchedCompletedTasks.length;
+  // Usamos las variables de tareas totales para el resumen
+  const pendingTasksSummary = pendingTasks.length;
+  const completedTasksSummary = completedTasks.length;
+
+  const addTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const toggleTask = (id) => {
+    return setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const handleFilterChange = (status) => {
+    setFilter(status);
+  };
   return (
     <>
       <Header search={search} setSearch={setSearch} />
