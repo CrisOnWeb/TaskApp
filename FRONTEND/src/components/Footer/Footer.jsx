75498cba7e5b87605d650deaa3a49e30b6f0f9e3
Cris <cristinaporteiro@gmail.com>
2026-07-26 12:00:35 +0200

    Added menu in footer component.

--- FRONTEND/src/components/Footer/Footer.jsx
+++ FRONTEND/src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import './Footer.scss';
+import { NavLink } from 'react-router-dom';
 
 const Footer = () => {
   return (
@@ -15,6 +16,45 @@ const Footer = () => {
             @CrisOnWeb
           </a>
         </p>
+        <nav className="footer__nav">
+          <ul className="footer__list">
+            <li className="footer__item">
+              <NavLink className="footer__link" to="/privacy">
+                Privacidad
+              </NavLink>
+            </li>
+            <li className="footer__item">
+              <NavLink className="footer__link" to="/terms">
+                Términos
+              </NavLink>
+            </li>
+            <li className="footer__item">
+              <NavLink className="footer__link" to="/contact">
+                Contacto
+              </NavLink>
+            </li>
+            <li className="footer__item">
+              <a
+                className="footer__link"
+                href="www.linkedin.com/in/cristinaporteiro"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li className="footer__item">
+              <a
+                className="footer__link"
+                href="https://github.com/CrisOnWeb/TaskApp"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </nav>
       </div>
     </footer>
   );
